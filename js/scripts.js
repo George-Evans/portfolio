170f54b3135224d91fb49e81552bfb1c9dfc74f4
George Evans <george.r.evans@googlemail.com>
2018-07-22 22:36:59 +0200

    homepage finished - testing

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 $(document).ready(function($) {
 
 	// Project Titles Hover
-	$(".overlay").mouseenter( function() {
-		$(".project-title", this).delay(200).fadeIn(500,"swing");
+	$(".project").mouseenter( function() {
+		$("img", this).delay(50).fadeTo(400,0);
+		$(".project-title", this).delay(300).fadeIn(500,"swing");
 	}).mouseleave( function() {
-		$(".project-title", this).hide();
+		$(".project-title", this).fadeOut(200);
+		$("img", this).delay(100).fadeTo(100,1);
 	});
 
 });
