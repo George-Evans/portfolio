3d5da4cf747d6dcd46f988b84d31951ebe8d69dd
George Evans <george.r.evans@googlemail.com>
2018-07-22 18:12:40 +0200

    removed header script, testing mobile site layout

--- js/scripts.js
+++ js/scripts.js
@@ -7,29 +7,4 @@ $(document).ready(function($) {
 		$(".project-title", this).hide();
 	});
 
-	// Hide Header on on scroll down
-	$(document).ready(function () {
-  
-	  'use strict';
-	  
-	   var c, currentScrollTop = 0,
-	       navbar = $('header');
-
-	   $(window).scroll(function () {
-	      var a = $(window).scrollTop();
-	      var b = navbar.height();
-	     
-	      currentScrollTop = a;
-	     
-	      if (c < currentScrollTop && a > b + b) {
-	        navbar.addClass("scrollUp");
-	      } else if (c > currentScrollTop && !(a <= b)) {
-	        navbar.removeClass("scrollUp");
-	      }
-	      c = currentScrollTop;
-	  });
-	  
-	});
-
-
 });
